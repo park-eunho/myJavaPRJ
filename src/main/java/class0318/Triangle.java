ef2a5d31a970aeabe43ba5f2d162dc43e5f2a28d
parkeunho <smart4gue@naver.com>
2024-03-20 14:49:39 +0900

    소스 업로드

---
+++ src/main/java/class0318/Triangle.java
@@ -0,0 +1,14 @@
+package class0318;
+
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.printf("*\n");
+        System.out.printf("**\n");
+        System.out.printf("***\n");
+        System.out.printf("****\n");
+        System.out.printf("***\n");
+        System.out.printf("**\n");
+        System.out.printf("*\n");
+
+    }
+}
