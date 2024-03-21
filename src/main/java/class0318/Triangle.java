0be32dc1e86bdab8bf4945c378f178f94d61838d
parkeunho <smart4gue@naver.com>
2024-03-21 17:06:21 +0900

    소스 수정

--- src/main/java/class0318/Triangle.java
+++ src/main/java/class0318/Triangle.java
@@ -2,13 +2,19 @@ package class0318;
 
 public class Triangle {
     public static void main(String[] args) {
-        System.out.printf("*\n");
-        System.out.printf("**\n");
-        System.out.printf("***\n");
-        System.out.printf("****\n");
-        System.out.printf("***\n");
-        System.out.printf("**\n");
-        System.out.printf("*\n");
-
+        for (int i = 1; i < 8; i++) {
+            if ( i <= 4) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+            }
+                else
+                {
+                    for ( int j = 0; j < 8 - i; j++) {
+                        System.out.print("*");
+                    }
+                }
+            System.out.println();
+            }
+        }
     }
-}
