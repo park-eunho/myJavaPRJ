f82d516f60c309225bde15a87ce0d3598f0cfa8a
parkeunho <smart4gue@naver.com>
2024-03-25 14:23:25 +0900

    소스 수정

--- src/main/java/Code04_13.java
+++ src/main/java/Code04_13.java
@@ -4,10 +4,12 @@ public class Code04_13 {
         String upper = "";
         String lower = "";
         String cutStr = "";
+        String allStr = "";
 
         upper = str.toUpperCase();
         lower = str.toLowerCase();
         cutStr = str.trim();
+        allStr = str.replaceAll(" ", "");
 
         System.out.println("기존 문자열 ==> [" + str + "]");
         System.out.println("대문자 ==> ["+ upper +"]");
