9d3d4f614c0d4e31a5ac473285be1534a95a64fe
parkeunho <smart4gue@naver.com>
2024-03-27 14:37:27 +0900

    소스 업로드

---
+++ src/main/java/class0318/practice4.java
@@ -0,0 +1,11 @@
+package class0318;
+
+public class practice4 {
+    public static void main(String[] args) {
+        String str = "난생처음 자바를 처음 학습중.자바는 처음이지만. 즐거움. ";
+
+        System.out.println(str.indexOf("처음"));
+        System.out.println(str.lastIndexOf("처음"));
+        System.out.println(str.indexOf("처음" , 4));
+    }
+}
