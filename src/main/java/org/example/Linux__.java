90b249f53d29b5011bf25890d3bcac107ae5ee72
parkeunho <smart4gue@naver.com>
2024-03-29 11:31:56 +0900

    소스 업로드

---
+++ src/main/java/org/example/Linux__.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import com.sun.javafx.scene.control.behavior.SliderBehavior;
+
+public class Linux__ {
+    cd
+    pwd
+    cd ../usr/lib
+    cd ../home/ubuntu
+    ls -l /etc/hosts
+    groups root
+    mkdir ch5
+    cd ch5
+    touch test.txt
+    ls -l
+    chmod u-w test.txt
+    ls -l
+}
