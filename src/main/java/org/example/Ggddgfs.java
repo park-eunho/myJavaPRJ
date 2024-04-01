d33f6d2272ca88e1b9852403285e642b603c98a1
parkeunho <smart4gue@naver.com>
2024-04-01 13:11:22 +0900

    소스 업로드

---
+++ src/main/java/org/example/Ggddgfs.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ggddgfs {
+    public static void main(String[] args) {
+        int a = 100;
+        Scanner s = new Scanner(System.in);
+        int b = 200;
+        int c = a * b;
+        System.out.println(c);
+    }
+}
