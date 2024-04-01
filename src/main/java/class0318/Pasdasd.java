f4f282d1a9c0457627702b3e498949f3d4cca3d9
parkeunho <smart4gue@naver.com>
2024-04-01 14:50:25 +0900

    소스 업로드

---
+++ src/main/java/class0318/Pasdasd.java
@@ -0,0 +1,14 @@
+package class0318;
+
+import java.util.Scanner;
+
+public class Pasdasd {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a, b, c;
+        a = s.nextInt();
+        b = s.nextInt();
+        c = a * b;
+        System.out.println(a + "*" + b + "=" + c);
+    }
+}
