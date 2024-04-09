3f78d972e515a726af6f4191322fd230bd779063
parkeunho <smart4gue@naver.com>
2024-04-09 15:23:34 +0900

    소스 업로드

---
+++ src/main/java/Class0401/Quizzzzzzzzzzzzzz.java
@@ -0,0 +1,28 @@
+package Class0401;
+
+import java.util.Scanner;
+
+public class Quizzzzzzzzzzzzzz {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        for (int i = a ; i < b+1 ; i++){
+            System.out.println(i);
+        }
+        if(a>b){
+            for (int i = a; i > b; i--){
+            if ( i % 3 ==0){
+                System.out.println(i);
+            }
+            } else {
+                for (int i = a; i < b; i++){
+                    if (i % 3 == 0){
+                        System.out.println(i);
+                    }
+                }
+            }
+
+        }
+    }
+}
