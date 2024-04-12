539ed4b32a4943a60dec0f0a282b56dcb7b23e92
parkeunho <smart4gue@naver.com>
2024-04-12 11:27:03 +0900

    소스 업로드

---
+++ src/main/java/My_practice/OUasdwq.java
@@ -0,0 +1,15 @@
+package My_practice;
+
+import java.util.Scanner;
+
+public class OUasdwq {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine();
+        int a = s.nextInt();
+
+        System.out.println(str.charAt(a-1));
+
+    }
+}
