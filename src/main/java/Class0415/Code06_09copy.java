f5effef1b86caf67748a49c18bd74734393cc6df
parkeunho <smart4gue@naver.com>
2024-04-15 15:40:53 +0900

    소스 업로드

---
+++ src/main/java/Class0415/Code06_09copy.java
@@ -0,0 +1,11 @@
+package Class0415;
+
+public class Code06_09copy {
+    public static void main(String[] args) {
+        for(int i=2; i<10; i++){
+            for(int j =1; j<10; j++){
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+        }
+    }
+}
---
+++ src/main/java/Class0415/Code06_13.java
@@ -0,0 +1,12 @@
+package Class0415;
+
+public class Code06_13 {
+    public static void main(String[] args) {
+
+        for(int i=0; i<1000; i++){
+            System.out.println(i + " : 반복문을 실행합니다");
+            break;
+        }
+    }
+}
+
