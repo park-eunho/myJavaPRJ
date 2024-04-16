5fb07cd1f6e2919f192f0b5f3c0be9eedee09ca6
parkeunho <smart4gue@naver.com>
2024-04-16 16:59:25 +0900

    소스 업로드

---
+++ src/main/java/My_practice/qwdqwq1r2.java
@@ -0,0 +1,13 @@
+package My_practice;
+
+import java.util.Scanner;
+
+public class qwdqwq1r2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int y= s.nextInt();
+        int hap;
+        hap=y-543;
+        System.out.println(hap);
+    }
+}
