40d0d416ddf53384a21b644aa65fae46d3f27bf9
parkeunho <smart4gue@naver.com>
2024-04-16 15:15:46 +0900

    소스 업로드

---
+++ src/main/java/My_practice/Disezmine.java
@@ -0,0 +1,11 @@
+package My_practice;
+
+public class Disezmine {
+    public static void main(String[] args) {
+        for(int i=1; i<10; i++){
+            for(int j=1; j<=9; j++){
+                System.out.println(i + " * " + j + " = " + i*j);
+            }
+        }
+    }
+}
