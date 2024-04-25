bdff3cb989b84590c2bf6e0b1a5a0ea2e90b6faf
parkeunho <smart4gue@naver.com>
2024-04-25 15:50:22 +0900

    소스 업로드

---
+++ src/main/java/My_practice/LASD.java
@@ -0,0 +1,13 @@
+package My_practice;
+
+import java.util.Scanner;
+
+public class LASD {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        long a=s.nextLong();
+        long b=s.nextLong();
+        long c=s.nextLong();
+        System.out.println(a+b+c);
+    }
+}
