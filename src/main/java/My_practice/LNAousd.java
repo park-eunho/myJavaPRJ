1dfa9abce563fea68cdaeff4087cf20dbb84ccef
parkeunho <smart4gue@naver.com>
2024-04-26 12:41:32 +0900

    소스 업로드

---
+++ src/main/java/My_practice/LNAousd.java
@@ -0,0 +1,10 @@
+package My_practice;
+
+public class LNAousd {
+    public static void main(String[] args) {
+        System.out.println("\\   /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
